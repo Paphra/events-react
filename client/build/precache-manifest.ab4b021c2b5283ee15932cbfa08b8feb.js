8fca995d6d55c60769491945bb44845f35f58a48
Epaphradito Lugayavu <paphra.me@gmail.com>
2020-03-29 02:48:45 +0300

    All New

---
+++ client/build/precache-manifest.ab4b021c2b5283ee15932cbfa08b8feb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583a044ccca2797e70cd135a5f2747f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a79c9dc8a4d87fff6336",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2e5229e984aa0ce6b335",
+    "url": "/static/css/main.a4b856aa.chunk.css"
+  },
+  {
+    "revision": "a79c9dc8a4d87fff6336",
+    "url": "/static/js/2.97af26a7.chunk.js"
+  },
+  {
+    "revision": "5a93dc6638418c12f3e8f19645777857",
+    "url": "/static/js/2.97af26a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e5229e984aa0ce6b335",
+    "url": "/static/js/main.a20804ed.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "786da41d1acd3e5c84666141a45876f6",
+    "url": "/static/media/logo.786da41d.png"
+  }
+]);
